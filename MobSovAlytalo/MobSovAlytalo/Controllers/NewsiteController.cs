5428720a74ced7827a264a38da368feb6dbf8be7
Timo Tirkkonen <timo.tirkkonen@student.careeria.fi>
2020-01-29 15:05:32 +0200

    css add

---
+++ MobSovAlytalo/MobSovAlytalo/Controllers/NewsiteController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MobSovAlytalo.Controllers
+{
+    public class NewsiteController : Controller
+    {
+        // GET: Newsite
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CreateAccount()
+        {
+            return View();
+        }
+
+        public ActionResult Something()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
